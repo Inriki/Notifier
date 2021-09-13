2889441f92b7884acea4322f686e7c52f446fa08
Israel Fernández Flores <israelfernandezflores@gmail.com>
2021-09-13 13:37:00 +0200

    Agregar archivos de proyecto.

---
+++ Notifier/Hubs/NotifierHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Notifier.Hubs;
+
+public static class UserHandler
+{
+    public static HashSet<string> ConnectedIds = new HashSet<string>();
+}
+    
+public class NotifierInfo
+{
+    public int ReadersCount { get; set; }
+    public List<string> Readers { get; internal set; } = null!;
+}
+2
+
+public class NotifierHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.Others.SendAsync("ReceiveMessage", message);
+    }
+
+    public async Task GetInfo()
+    {
+        var readers = UserHandler.ConnectedIds.Where(c => c != Context.ConnectionId).ToList();
+        NotifierInfo info = new NotifierInfo { ReadersCount = readers.Count, Readers = readers };
+        await Clients.Caller.SendAsync("ReceiveInfo", info);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        UserHandler.ConnectedIds.Add(Context.ConnectionId);
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+        return base.OnDisconnectedAsync(exception);
+    }
+}
---
+++ Notifier/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using Notifier.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<NotifierHub>("/notifierHub");
+});
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
